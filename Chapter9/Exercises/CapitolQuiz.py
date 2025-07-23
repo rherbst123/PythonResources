d24376142937cf97ef410ec82683b27b019625e7
Riley Herbst <rileyherbst@yahoo.com>
2025-07-23 10:08:33 -0500

    Finishing

--- Chapter9/Exercises/CapitolQuiz.py
+++ Chapter9/Exercises/CapitolQuiz.py
@@ -7,64 +7,64 @@
 # countries and their capitals.
 import random
 
-# states_and_capitals = {
-#     'Alabama': 'Montgomery',
-#     'Alaska': 'Juneau',
-#     'Arizona': 'Phoenix',
-#     'Arkansas': 'Little Rock',
-#     'California': 'Sacramento',
-#     'Colorado': 'Denver',
-#     'Connecticut': 'Hartford',
-#     'Delaware': 'Dover',
-#     'Florida': 'Tallahassee',
-#     'Georgia': 'Atlanta',
-#     'Hawaii': 'Honolulu',
-#     'Idaho': 'Boise',
-#     'Illinois': 'Springfield',
-#     'Indiana': 'Indianapolis',
-#     'Iowa': 'Des Moines',
-#     'Kansas': 'Topeka',
-#     'Kentucky': 'Frankfort',
-#     'Louisiana': 'Baton Rouge',
-#     'Maine': 'Augusta',
-#     'Maryland': 'Annapolis',
-#     'Massachusetts': 'Boston',
-#     'Michigan': 'Lansing',
-#     'Minnesota': 'Saint Paul',
-#     'Mississippi': 'Jackson',
-#     'Missouri': 'Jefferson City',
-#     'Montana': 'Helena',
-#     'Nebraska': 'Lincoln',
-#     'Nevada': 'Carson City',
-#     'New Hampshire': 'Concord',
-#     'New Jersey': 'Trenton',
-#     'New Mexico': 'Santa Fe',
-#     'New York': 'Albany',
-#     'North Carolina': 'Raleigh',
-#     'North Dakota': 'Bismarck',
-#     'Ohio': 'Columbus',
-#     'Oklahoma': 'Oklahoma City',
-#     'Oregon': 'Salem',
-#     'Pennsylvania': 'Harrisburg',
-#     'Rhode Island': 'Providence',
-#     'South Carolina': 'Columbia',
-#     'South Dakota': 'Pierre',
-#     'Tennessee': 'Nashville',
-#     'Texas': 'Austin',
-#     'Utah': 'Salt Lake City',
-#     'Vermont': 'Montpelier',
-#     'Virginia': 'Richmond',
-#     'Washington': 'Olympia',
-#     'West Virginia': 'Charleston',
-#     'Wisconsin': 'Madison',
-#     'Wyoming': 'Cheyenne'
-# }
-
 states_and_capitals = {
     'Alabama': 'Montgomery',
-    'Alaska': 'Juneau'
-
+    'Alaska': 'Juneau',
+    'Arizona': 'Phoenix',
+    'Arkansas': 'Little Rock',
+    'California': 'Sacramento',
+    'Colorado': 'Denver',
+    'Connecticut': 'Hartford',
+    'Delaware': 'Dover',
+    'Florida': 'Tallahassee',
+    'Georgia': 'Atlanta',
+    'Hawaii': 'Honolulu',
+    'Idaho': 'Boise',
+    'Illinois': 'Springfield',
+    'Indiana': 'Indianapolis',
+    'Iowa': 'Des Moines',
+    'Kansas': 'Topeka',
+    'Kentucky': 'Frankfort',
+    'Louisiana': 'Baton Rouge',
+    'Maine': 'Augusta',
+    'Maryland': 'Annapolis',
+    'Massachusetts': 'Boston',
+    'Michigan': 'Lansing',
+    'Minnesota': 'Saint Paul',
+    'Mississippi': 'Jackson',
+    'Missouri': 'Jefferson City',
+    'Montana': 'Helena',
+    'Nebraska': 'Lincoln',
+    'Nevada': 'Carson City',
+    'New Hampshire': 'Concord',
+    'New Jersey': 'Trenton',
+    'New Mexico': 'Santa Fe',
+    'New York': 'Albany',
+    'North Carolina': 'Raleigh',
+    'North Dakota': 'Bismarck',
+    'Ohio': 'Columbus',
+    'Oklahoma': 'Oklahoma City',
+    'Oregon': 'Salem',
+    'Pennsylvania': 'Harrisburg',
+    'Rhode Island': 'Providence',
+    'South Carolina': 'Columbia',
+    'South Dakota': 'Pierre',
+    'Tennessee': 'Nashville',
+    'Texas': 'Austin',
+    'Utah': 'Salt Lake City',
+    'Vermont': 'Montpelier',
+    'Virginia': 'Richmond',
+    'Washington': 'Olympia',
+    'West Virginia': 'Charleston',
+    'Wisconsin': 'Madison',
+    'Wyoming': 'Cheyenne'
 }
+
+# states_and_capitals = {
+#     'Alabama': 'Montgomery',
+#     'Alaska': 'Juneau'
+
+# }
 
 
 
@@ -73,5 +73,14 @@ wrong = 0
 right = 0
 
 for key in states_and_capitals.keys():
+    print(f"What is the capital of {key}?")
+    answer = input()
+    if answer == states_and_capitals[key]:
+        print("Correct!")
+        right += 1
+    else:
+        print("Incorrect!")
+        wrong += 1
 
+print(f"You got {right} correct and {wrong} wrong.")
 
