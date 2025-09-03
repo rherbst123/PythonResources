a3f473cc826274045fc37da369cfbfd4ab707d69
Riley Herbst <rileyherbst@yahoo.com>
2025-09-03 11:18:59 -0500

    Chapter 2 Examples

---
+++ In_Class/Chapter2(InputOutput)/Input.py
@@ -0,0 +1,4 @@
+#camelCase input for variable name
+foodOption = input("What food Option Do You Want?: ")
+
+print("Your Food Option is: ",foodOption)
---
+++ In_Class/Chapter2(InputOutput)/Line_By_Line.py
@@ -0,0 +1,7 @@
+#See how 123 is completly ignored?
+x = 123
+
+x = 456
+
+print(x)
+
